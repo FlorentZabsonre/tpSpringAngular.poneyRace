6c6bbe80381f79b06b950773be722ab7c4725f2c
FlorentZabsonre <florent@gmail.com>
2018-11-07 11:47:04 +0100

    modif du raceController

--- src/main/java/dta/spring/tpSpringAngular/poneyRace/Controller/RaceController.java
+++ src/main/java/dta/spring/tpSpringAngular/poneyRace/Controller/RaceController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,38 +25,41 @@ public class RaceController {
 	@Autowired
 	RaceRepository raceRepo;
 
+	@CrossOrigin(origins = "*")
 	@GetMapping("/")
-	public List<Race> getAll(){
+	public List<Race> getAll() {
 		return raceRepo.findAll();
 	}
 
+	@CrossOrigin(origins = "*")
 	@GetMapping("/{id}")
-	public Race getById(@PathVariable(value="id") Long raceId){
-		return raceRepo.findById(raceId)
-				.orElseThrow(() ->new ResourceNotFoundException("race", "id", raceId));
+	public Race getById(@PathVariable(value = "id") Long raceId) {
+		return raceRepo.findById(raceId).orElseThrow(() -> new ResourceNotFoundException("race", "id", raceId));
 	}
 
-
-	@PostMapping(path="/")
+	@CrossOrigin(origins = "*")
+	@PostMapping(path = "/")
 	public Race create(@RequestBody Race race) {
 
 		return raceRepo.save(race);
 	}
 
+	@CrossOrigin(origins = "*")
 	@PutMapping("/{id}")
-	public Race updateBy (@PathVariable(value="id") Long raceId,@RequestBody Race race)
-	{	List <Pony> ponies = new ArrayList<>();
-	Race r= raceRepo.findById(raceId).orElseThrow(() -> new ResourceNotFoundException("race", "id", raceId));
-	r.setDateR(race.getDateR());
-	r.setLocation(race.getLocation());
-	r.setPonies(race.getPonies());
-	Race raceUp = raceRepo.save(r);
-	return raceUp;
+	public Race updateBy(@PathVariable(value = "id") Long raceId, @RequestBody Race race) {
+		List<Pony> ponies = new ArrayList<>();
+		Race r = raceRepo.findById(raceId).orElseThrow(() -> new ResourceNotFoundException("race", "id", raceId));
+		r.setDateR(race.getDateR());
+		r.setLocation(race.getLocation());
+		r.setPonies(race.getPonies());
+		Race raceUp = raceRepo.save(r);
+		return raceUp;
 	}
+
+	@CrossOrigin(origins = "*")
 	@DeleteMapping("/{id}")
 	public void deleteById(@PathVariable(value = "id") Long raceId) {
-		Race p =  raceRepo.findById(raceId)
-				.orElseThrow(() -> new ResourceNotFoundException("pony", "id", raceId));
+		Race p = raceRepo.findById(raceId).orElseThrow(() -> new ResourceNotFoundException("pony", "id", raceId));
 		raceRepo.delete(p);
 	}
 }
